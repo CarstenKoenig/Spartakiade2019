c9169880c28fcdccc2e501039b7668d6e5fe4831
Carsten König <Carsten.Koenig@hotmail.de>
2019-04-28 21:49:35 +0200

    Concur-Demo reset

--- Projects/Concur/src/Main.purs
+++ Projects/Concur/src/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import Concur.Core (Widget)
 import Concur.React (HTML)
 import Concur.React.DOM as D
+import Concur.React.Props (className)
 import Concur.React.Props as P
 import Concur.React.Run (runWidgetInDom)
 import Effect (Effect)
@@ -18,10 +19,12 @@ main = runWidgetInDom "widget" $ counterWidget 0
 counterWidget :: forall a. Int -> Widget HTML a
 counterWidget count = do
   n <- D.div'
-        [ D.p' [D.text ("State: " <> show count)]
-        , D.button [P.onClick] [D.text "Increment"] $> count+1
+        [ D.p' [D.text ("Wert: " <> show count)]
+        , map (\_ -> count+1) (D.button [ className "test", P.onClick] [D.text "Increment"])
         , D.button [P.onClick] [D.text "Decrement"] $> count-1
+        , D.button [P.onClick] [D.text "reset"] $> 0
         ]
 
   liftEffect (log ("COUNT IS NOW: " <> show n))
+
   counterWidget n
